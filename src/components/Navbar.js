e15184657dfe3d54b5375d846f534b09c5435b7d
andrewtlu <andrewlulu2012@gmail.com>
2023-10-22 01:39:55 -0400

    Make Navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import '../styles/Navbar.css'
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  // TODO: hamburger flippy thingy (https://www.youtube.com/watch?v=gAGcjlJyKk0)
+  // TODO: smooth open
+
   return (
     <nav>
-      <div className='navbar'>
-        <NavLink to='/'>
-          <div className='logo'>
-            {/* Add logo here */}
-            atl
+      <div className='navbar' id={expandNavbar ? 'open' : 'close'}>
+        {/* combine logo/hamburger for easier responsive */}
+        <div class='main'>
+          <NavLink to='/'>
+            <div className='logo'>
+              {/* Add logo here */}
+              atl
+            </div>
+          </NavLink>
+
+          <div className='toggleMenu' onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}>
+            <div className='burger'></div>
+            <div className='burger'></div>
           </div>
-        </NavLink>
-        <div className='toggleMenu'>
-          {/* Add hamburger here */}
-          toggleMenu
         </div>
+        
         <ul className='links'>
           <NavLink to='/about'><li>about</li></NavLink>
           <NavLink to='/projects'><li>projects</li></NavLink>
