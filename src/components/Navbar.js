a1fe5e513a02cc4f852eae8f11fef3664abe3e25
andrewtlu <andrewlulu2012@gmail.com>
2023-10-22 16:40:40 -0400

    Finish responsive navbar (nav drop, spinny thing)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,18 @@ import '../styles/Navbar.css'
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
+  const [transition, setTransition] = useState(false);
+  const links = document.querySelector('.links');
+
+  window.addEventListener('resize', function() { // disable transition when resizing
+    setTransition(false);
+  });
   // TODO: hamburger flippy thingy (https://www.youtube.com/watch?v=gAGcjlJyKk0)
   // TODO: smooth open
 
   return (
     <nav>
-      <div className='navbar' id={expandNavbar ? 'open' : 'close'}>
+      <div className='navbar'>
         {/* combine logo/hamburger for easier responsive */}
         <div class='main'>
           <NavLink to='/'>
@@ -21,13 +27,14 @@ function Navbar() {
 
           <div className='toggleMenu' onClick={() => {
             setExpandNavbar((prev) => !prev);
-          }}>
-            <div className='burger'></div>
-            <div className='burger'></div>
+            setTransition(true);
+          }} id={expandNavbar && 'active'}>
+            <div className='burger top'></div>
+            <div className='burger bottom'></div>
           </div>
         </div>
         
-        <ul className='links'>
+        <ul className={'links ' + (transition && 'transition')} id={expandNavbar && 'active'}>
           <NavLink to='/about'><li>about</li></NavLink>
           <NavLink to='/projects'><li>projects</li></NavLink>
           <NavLink to='/contact'><li>contact</li></NavLink>
