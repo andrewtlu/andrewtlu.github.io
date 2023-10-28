341fae542582c1526def49cb8a9d18223750ed33
andrewtlu <andrewlulu2012@gmail.com>
2023-10-27 23:56:16 -0400

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/Footer.css'
+import { Link } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='content'>
+            <div className='message'>
+                <p><a target='_blank' href='https://github.com/andrewtlu/andrewtlu.github.io'>Made with &lt;3</a></p>
+                <p><a target='_blank' href='https://github.com/andrewtlu'>&copy;2023 Andrew Lu</a></p>
+            </div>
+            <ul className='foot-links'>
+                <Link to='/about'><li>about</li></Link>
+                <Link to='/projects'><li>projects</li></Link>
+                <Link to='/contact'><li>contact</li></Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
