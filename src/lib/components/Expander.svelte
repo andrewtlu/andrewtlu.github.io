482324c265f6818fa6ccaf46b6512e9f57fc2bb3
andrewtlu <andrewlulu2012@gmail.com>
2026-05-29 22:37:08 +1000

    Add initial webpage

---
+++ src/lib/components/Expander.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let { display, content }: { display: string; content: HTMLElement[] } = $props();
+</script>
+
+<div class="whitespace-pre">
+    {display}
+    {#each content as h, idx (idx)}
+        {h}
+    {/each}
+</div>
