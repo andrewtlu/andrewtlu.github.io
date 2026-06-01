77f8dbf74c39f625c083089ff526899532f4a42c
andrewtlu <andrewlulu2012@gmail.com>
2026-06-01 23:16:52 +1000

    Add first making of post

--- src/lib/yap/index.ts
+++ src/lib/yap/index.ts
@@ -1,5 +1,5 @@
 import Welcome, { metadata as welcomeMeta } from "$lib/yap/welcome.svx";
-import MakingOf, { metadata as makingOfMeta } from "$lib/yap/making-of.svx";
+import MakingOfPt1, { metadata as makingOfMetaPt1 } from "$lib/yap/making-of-pt1.svx";
 import type { Component } from "svelte";
 
 export type Post = {
@@ -19,11 +19,11 @@ const posts: Post[] = [
     raw: welcomeMeta.raw,
   },
   {
-    component: MakingOf,
-    title: makingOfMeta.title,
-    date: new Date(makingOfMeta.date),
-    posted: new Date(makingOfMeta.posted),
-    raw: makingOfMeta.raw,
+    component: MakingOfPt1,
+    title: makingOfMetaPt1.title,
+    date: new Date(makingOfMetaPt1.date),
+    posted: new Date(makingOfMetaPt1.posted),
+    raw: makingOfMetaPt1.raw,
   },
 ];
 
