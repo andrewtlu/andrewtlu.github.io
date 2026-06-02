03dcd67151403ea74b2faf70e84442c78d9ae181
andrewtlu <andrewlulu2012@gmail.com>
2026-06-02 15:29:21 +1000

    Add pt2 post

--- src/lib/yap/index.ts
+++ src/lib/yap/index.ts
@@ -1,5 +1,6 @@
 import Welcome, { metadata as welcomeMeta } from "$lib/yap/welcome.svx";
 import MakingOfPt1, { metadata as makingOfMetaPt1 } from "$lib/yap/making-of-pt1.svx";
+import MakingOfPt2, { metadata as makingOfMetaPt2 } from "$lib/yap/making-of-pt2.svx";
 import type { Component } from "svelte";
 
 export type Post = {
@@ -24,6 +25,13 @@ const posts: Post[] = [
     date: new Date(makingOfMetaPt1.date),
     posted: new Date(makingOfMetaPt1.posted),
     raw: makingOfMetaPt1.raw,
+  },
+  {
+    component: MakingOfPt2,
+    title: makingOfMetaPt2.title,
+    date: new Date(makingOfMetaPt2.date),
+    posted: new Date(makingOfMetaPt2.posted),
+    raw: makingOfMetaPt2.raw,
   },
 ];
 
